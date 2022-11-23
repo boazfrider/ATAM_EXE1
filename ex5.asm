bd6a8fc19dc9fd68c048706d3ce96aab453b65e3
Daniel Belokon <daniel.belokon@gmail.com>
2022-11-23 19:25:29 +0200

    implement EX5

--- ex5.asm
+++ ex5.asm
@@ -2,4 +2,63 @@
 
 .section .text
 _start:
-#your code here
+
+# rax - prev of first occurence 
+
+# rbx - prev of the second occurence
+
+# (%rsp) - current node value
+# 4(%rsp) - next node
+
+# %r9 - counter. 
+
+    mov $0, %r9
+    mov $0, %rax
+    mov $0, %rbx
+
+    mov (head), %rsi
+    movq $0, %rdi # pointer for prev.
+    
+.iterateOverList:
+
+    movslq (val), %r10
+    cmpq %r10, (%rsi)
+    jne .nextNode
+
+    inc %r9
+    cmpq $2, %r9
+    jg .end
+    cmpq $1, %r9
+    jne .secondOccurence
+# first occurance
+    movq %rdi, %rax
+    movq %rsi, %rcx
+    jmp .nextNode
+    
+.secondOccurence:
+    movq %rdi, %rbx
+    movq %rsi, %rdx
+    jmp .nextNode
+
+# move to the next node
+
+.nextNode:
+    movq %rsi, %rdi
+    movq 8(%rsi), %rsi
+    cmpq $0, %rsi
+    jne .iterateOverList
+
+
+    cmp $2, %r9
+    jne .end
+.swap:
+    movq %rdx, 8(%rax) 
+    movq %rcx, 8(%rbx)
+    movq 8(%rdx), %r10
+    movq 8(%rcx), %r11
+    movq %r10, 8(%rcx)
+    movq %r11, 8(%rdx)
+
+.end:
+
+    
