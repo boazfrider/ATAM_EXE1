14df7aa51177167c808ed87b4b310e8b20d54488
Daniel Belokon <daniel.belokon@gmail.com>
2022-11-25 18:20:56 +0200

    fix ex2

--- ex2.asm
+++ ex2.asm
@@ -9,21 +9,25 @@ _start:
 
     #num = %eax
 
-    #srcptr = %ecx
-    #srcdir = %edx
+    # srcptr = %ecx
+    # srcdir = %edx
 
-    #destptr = %esi
-    #destdir = %r8
+    # destptr = %esi
+    # destdir = %r8
+    # reverse/negative flag = %r10b
     
     mov $0, %rax
     mov (num), %eax
+    mov $0, %r10b
     cmp $0, %eax
     jge .numPositive # if num >= 0, jump to .numPositive
 
     # make num positive
     xor $0xFFFFFFFF, %eax
     add $1, %eax 
-    jmp .numNegative
+    # set negative flag
+    mov $1, %r10b
+
 .numPositive:
     mov $destination, %rbx
     sub $source, %rbx
@@ -47,29 +51,6 @@ _start:
     mov $1, %r8d
     jmp loop_start
 
-   
-.numNegative:
-    mov $source, %rbx
-    sub $destination, %rbx
-    cmp $0, %rbx 
-    jl .negativeNotOverLapping
-    cmpq %rax, %rbx
-    jge .negativeNotOverLapping
-    mov $0, %ecx
-    mov $1, %edx
-    mov %eax, %esi
-    dec %esi
-    mov $-1, %r8d
-    jmp loop_start
-
-.negativeNotOverLapping:
-    mov %eax, %ecx
-    dec %ecx
-    mov $-1, %edx
-    mov $0, %esi
-    mov $1, %r8d
-    jmp loop_start
-
 loop_start:
     mov $0, %r9d
 loop:
@@ -85,5 +66,24 @@ loop:
     jmp loop
     
 loop_end:
+
+    cmp $0, %r10b
+    je .end
+    # reverse n bytes of destination
+    mov $0, %r9d
+    dec %eax
+reverse_loop:
+    cmp %r9d, %eax
+    jle .end
+    movb destination(%r9d), %bl
+    movb destination(%eax), %bh
+    movb %bl, destination(%eax)
+    shr $8, %bx
+    movb %bl, destination(%r9d)
+    addl $1, %r9d
+    decl %eax
+    jmp reverse_loop
+
+.end:
 
     
